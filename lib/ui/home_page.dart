223517081bf8648921c90954494cf653d83062f7
bhismaSatwika <bhisma.sawtika@gmail.com>
2022-01-06 20:21:46 +0800

    update map with google map api

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/plugin_api.dart';
-import 'package:latlong2/latlong.dart';
 import 'package:zonain/common/style.dart';
-import 'package:zonain/model/user_location.dart';
 import 'package:zonain/services/user_location_services.dart';
+import 'package:zonain/widget/map_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -15,7 +13,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   UserLocationService userLocationService = UserLocationService();
-
+  
   @override
   void dispose() {
     userLocationService.dispose();
@@ -24,91 +22,80 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final mapHeight = MediaQuery.of(context).size.height - 350;
-
+    final width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: CustomScrollView(
-        physics: const NeverScrollableScrollPhysics(),
-        slivers: [
-          SliverAppBar(
-            backgroundColor: background,
-            leading: Container(
-              margin: const EdgeInsets.only(left: 8, top: 8),
-              child: ClipRRect(
-                child: Image.network(
-                  'https://i.pinimg.com/originals/26/42/26/26422665b452967ebc301deadb2a036d.jpg',
-                ),
-                borderRadius: BorderRadius.circular(15),
-              ),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: background,
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ClipRRect(
+            child: Image.network(
+              'https://i.pinimg.com/originals/26/42/26/26422665b452967ebc301deadb2a036d.jpg',
             ),
-            title: const Text('Uhsheuppp'),
-            expandedHeight: 150,
-            flexibleSpace: Padding(
-              padding: const EdgeInsets.only(top: 100),
-              child: Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('Alarm'),
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(Colors.red),
-                        shape: MaterialStateProperty.all(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                        ),
-                        padding: MaterialStateProperty.all(
-                          const EdgeInsets.symmetric(
-                            horizontal: 40,
-                          ),
-                        ),
+            borderRadius: BorderRadius.circular(15),
+          ),
+        ),
+        title: const Text('Bhisma'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            color: background,
+            width: width,
+            height: 80,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Alarm'),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.red),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
                       ),
                     ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('Emergency call'),
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(Colors.red),
-                        shape: MaterialStateProperty.all(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                        ),
-                        padding: MaterialStateProperty.all(
-                          const EdgeInsets.symmetric(
-                            horizontal: 30,
-                          ),
-                        ),
+                    padding: MaterialStateProperty.all(
+                      const EdgeInsets.symmetric(
+                        horizontal: 40,
                       ),
                     ),
-                  ],
+                  ),
                 ),
-              ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Emergency call'),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.red),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    padding: MaterialStateProperty.all(
+                      const EdgeInsets.symmetric(
+                        horizontal: 30,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-          SliverToBoxAdapter(
-            child: SizedBox(
-              height: mapHeight,
-              child: StreamBuilder<UserLocation>(
-                stream: userLocationService.userLocationStream,
-                builder: (_, snapshot) => (snapshot.hasData)
-                    ? _mapWidget(
-                        snapshot.data!.latitude,
-                        snapshot.data!.longitude,
-                      )
-                    : _mapWidget(-8.409518, 115.188919),
-              ),
-            ),
+          const Expanded(
+            flex: 3,
+            child: MapWidget(),
           ),
-          SliverToBoxAdapter(
+          Expanded(
+            flex: 1,
             child: Container(
               color: background,
-              height: 200,
+              height: 100,
+              width: width,
               child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   ElevatedButton(
                     onPressed: () {},
@@ -150,37 +137,6 @@ class _HomePageState extends State<HomePage> {
           )
         ],
       ),
-    );
-  }
-
-  Widget _mapWidget(double latitude, double longitude) {
-    return FlutterMap(
-      options: MapOptions(
-        zoom: 13,
-        center: LatLng(latitude, longitude),
-      ),
-      layers: [
-        TileLayerOptions(
-          urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-          subdomains: ['a', 'b', 'c'],
-          attributionBuilder: (_) {
-            return const Text("Zonain");
-          },
-        ),
-        MarkerLayerOptions(
-          markers: [
-            Marker(
-              width: 80.0,
-              height: 80.0,
-              point: LatLng(latitude, longitude),
-              builder: (ctx) => const Icon(
-                Icons.location_on,
-                color: Colors.red,
-              ),
-            ),
-          ],
-        ),
-      ],
     );
   }
 }
